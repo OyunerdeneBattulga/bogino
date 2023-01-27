19dd2f79775c8c19f2b1784cfb18a3dedfe468bb
oyunerdene battulga <99385884+OyunerdeneBattulga@users.noreply.github.com>
2023-01-27 16:06:10 +0800

    loginoozas---

--- backend/Controller/usersController.js
+++ backend/Controller/usersController.js
@@ -5,46 +5,56 @@ const SECRET_KEY = 'placeholder_secret'
 const bcrypt = require("bcrypt")
 
 exports.signup = async (request, response, next) => {
+    // try {
+    //     const {password, email} = request.body;
+    //     const existingUser = await UserModel.findOne({ email:email });
+    //     if(existingUser) { 
+    //         return response.status(409).json({message:"butgeltei hereglegch baina"});
+    //     }
+    //     const salt = bcrypt.genSaltSync(10)
+    //     const hashedPassword = bcrypt.hashSync(password, salt)
+    //     const result = await UserModel.create({
+    //         email:email,
+    //         password:hashedPassword
+    //     });
+    //     const token = jwt.sign({email:result.email, id: result._id}, SECRET_KEY);
+    //     response.status(201).json({user:result, token:token});
+    // } catch(error){
+    //     response.status(500).json({message:"ymar negenzuil buruu baina"});
+    //     console.log(error);
+    // }
+}
+
+
+
+exports.login = async (req, res, next) => {
     try {
-        const {password, email} = request.body;
-        const existingUser = await UserModel.findOne({ email:email });
-        if(existingUser === email) { 
-            return response.status(409).json({message:"butgeltei hereglegch baina"});
-        }
-        const salt = bcrypt.genSaltSync(10)
-        const hashedPassword = bcrypt.hashSync(password, salt)
-        const result = await UserModel.create({
-            email:email,
-            password:hashedPassword
-        });
-        const token = jwt.sign({email:result.email, id: result._id}, SECRET_KEY);
-        response.status(201).json({user:result, token:token});
-    } catch(error){
-        console.log(error);
-        response.status(500).json({message:"ymar negenzuil buruu baina"});
+      const { email, password } = req.body;
+      console.log(email, password);
+      const existingUser = await User.findOne({
+        email,
+      });
+      if (!existingUser) {
+        return res
+          .status(404)
+          .json({ message: "email esvel nuuts ud buruu baina" });
+      }
+      const matchPassword = await bcrypt.compare(password, existingUser.password);
+      if (!matchPassword) {
+        return res
+          .status(404)
+          .json({ message: "email esvel nuuts ug buruu baina" });
+      }
+      const token = jwt.sign(
+        { email: existingUser.email, id: existingUser._id },
+        SECRET_KEY
+      );
+      res.status(201).json({ user: existingUser, token });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "ymar negen zuil buruu baina" });
     }
-}
-
-
-
-exports.login = async(request, response, next) => {
-    try{
-        const {email, password} = request.body;
-        const existingUser = await UserModel.findOne({password:password , email:email});
-        if(!existingUser){
-            return response.status(404).json({message:"email esvel nuuts ud buruu baina"});
-        }
-        const matchPassword = await bcrypt.compare(password, existingUser.password);
-        if(!matchPassword){
-            return response.status(404).json({message:"email esvel nuuts ug buruu baina"});
-        }
-        const token = jwt.sign({email:result.email, id:result._id}, SECRET_KEY);
-        response.status(201).json({user:existingUser, token: token});
-    } catch(error){
-        console.log(error);
-        response.status(500).json({message:"ymar negen zuil buruu baina"});
-    }
-}
+  };
 
 
 
